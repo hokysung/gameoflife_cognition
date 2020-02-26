6adf3a7bb02c31cbc595772c97e32a108fd5aa54
ynkwon <ynkwon@stanford.edu>
2020-02-25 16:01:42 -0800

    padding for boards

--- boardmaker.py
+++ boardmaker.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn as nn
 
 #f=open("./all/4boats.rle", "r")
 
@@ -48,6 +49,11 @@ def boardmaker(filename):
     a = lines[j].split(',')
     x = int(a[0][4:])
     y = int(a[1][5:])
+    
+    if x > 50 or y > 50:
+        return None
+
+
     board = torch.zeros(x*y)
 
     decoded = decoder(encoded,x,y)
@@ -59,7 +65,16 @@ def boardmaker(filename):
 
 
     board = board.reshape(y,x)
+    hor = 50 - x
+    ver = 50 - y
+    
+    pad = nn.ConstantPad2d((int(hor/2), hor -  int(hor/2), int(ver/2), ver-int(ver/2)), 0)
+    
+    board = pad(board)
+
+    print(board.shape)
+
     return board
 
 
-print(boardmaker("./all/4boats.rle"))
+print(boardmaker("./all/2x2glider.rle"))
